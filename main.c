f3c2dc08ddeb74297a5af02d78decbeac78a5c89
hepona <hepona10@gmail.com>
2023-06-20 15:07:37 -0700

    test

--- main.c
+++ main.c
@@ -9,12 +9,25 @@
 int main(int argc, char *argv[])
 {
 	FILE *file = fopen(argv[1], "r");
+	stack_t *list;
 
-	if (argc != 2 || file == NULL)
+	if (argc != 2 || argv[1] == NULL)
 	{
 		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+	if (file == NULL)
+	{
+		printf("Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	list = malloc(sizeof(stack_t));
+	if (list == NULL)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
 	fclose(file);
 	return (0);
 }
